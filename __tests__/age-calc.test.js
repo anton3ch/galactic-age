3946ef5928c8baee51304bea27b466354da57b82
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-14 11:39:18 -0600

    create test for GalacticAge method that returns user inputted age in Mercury years

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -1,9 +1,16 @@
 import {GalacticAge} from '../src/js/age-calc.js';
 
 describe('GalacticAge', () => {
-  test('should create galacticAge object with user input age parameter', () => {
+  beforeEach(() => { 
     let galacticAge = new GalacticAge(25);
+  });
+
+  test('should create galacticAge object with user input age parameter', () => {
     expect(galacticAge).toEqual({age: 25});
-    
+  });
+
+  test('should create GalacticAge method that returns user inputted age in Mercury years.', () => {
+    galacticAge.calcAge()
+    expect(galacticAge).toEqual({age: 25, mercuryAge: 6});
   });
 });
