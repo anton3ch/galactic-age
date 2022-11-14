94239c3fc0b147c1a1ace9f45ce4926bcd183f42
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-14 13:31:44 -0600

    create a method to calculate how much time user will live based on Neptune years

--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -22,5 +22,6 @@ export class GalacticAge {
     this.jupiterAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 11.86).toFixed(2));
     this.saturnAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 29.5).toFixed(2));
     this.uranusAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 84).toFixed(2));
+    this.neptuneAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 165).toFixed(2));
   }
 }
