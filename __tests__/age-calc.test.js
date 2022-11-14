2cfbbd276c9e8e203f0ce9064984728007f05502
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-14 12:04:43 -0600

    add a method to calculate age based on mercury year

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -1,8 +1,9 @@
 import {GalacticAge} from '../src/js/age-calc.js';
 
 describe('GalacticAge', () => {
+  let galacticAge;
   beforeEach(() => { 
-    let galacticAge = new GalacticAge(25);
+    galacticAge = new GalacticAge(25);
   });
 
   test('should create galacticAge object with user input age parameter', () => {
--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -2,4 +2,9 @@ export class GalacticAge {
   constructor(age) {
     this.age = age;
   }
+
+  calcAge() {
+    this.mercuryAge = this.age * 0.24;
+
+  }
 }
