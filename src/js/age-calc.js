6c2696a55546f573c1be8dec402921de9a9462c8
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-14 12:30:11 -0600

    change method to also calculate mars age

--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -6,5 +6,6 @@ export class GalacticAge {
   calcAge() {
     this.mercuryAge = (this.age * 365 / 88).toFixed(2);
     this.venusAge = (this.age * 365 / 225).toFixed(2);
+    this.marsAge = (this.age * 365 / 687).toFixed(2);
   }
 }
