306d3ed39624f7f5b29a296b4474a17f640d29a1
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-14 11:18:20 -0600

    add a test for galacticAge object

---
+++ __tests__/age-calc.test.js
@@ -0,0 +1,9 @@
+
+
+describe('GalacticAge', () => {
+  test('should create galacticAge object with user input age parameter', () => {
+    let galacticAge = new GalacticAge(25);
+    expect(galacticAge).toEqual({age: 25});
+    
+  });
+});
