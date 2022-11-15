4dd232be86417ed47fab71c110b78b2e954651fa
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-14 18:50:12 -0600

    create ui logic to display the results

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import {GalacticAge} from './js/age-calc.js';
+
+
+function handleLoad() {
+  document.getElementById("solar").addEventListener("click", function(event){
+    let inputId = event.target.id;
+    console.log(inputId);
+    let targetBox = document.getElementById(inputId + "Info");
+    console.log(targetBox);
+    // $(".text-left").not(targetBox).hide();
+    targetBox.show();
+  });
+}
+
+window.addEventListener("load", handleLoad);
+
+document.getElementById("user-form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  const userAge = document.getElementById("user-age").value;
+  const lifeExpect = document.getElementById("life-expect").value;
+  let galacticAge = new GalacticAge(userAge, lifeExpect);
+  galacticAge.calcAge();
+  galacticAge.calcExpectancy();
+
+  document.getElementById("mercuryAge").innerText = galacticAge.mercuryAge;
+  document.getElementById("mercuryExpect").innerText = galacticAge.mercuryAgeExpect;
+
+  document.getElementById("venusAge").innerText = galacticAge.venusAge;
+  document.getElementById("venusExpect").innerText = galacticAge.venusAgeExpect;
+
+  document.getElementById("earthExpect").innerText = userAge;
+  document.getElementById("earthAge").innerText = lifeExpect;
+
+  document.getElementById("marsAge").innerText = galacticAge.marsAge;
+  document.getElementById("marsExpect").innerText = galacticAge.marsAgeExpect;
+
+  document.getElementById("jupiterAge").innerText = galacticAge.jupiterAge;
+  document.getElementById("jupiterExpect").innerText = galacticAge.jupiterAgeExpect;
+
+  document.getElementById("saturnAge").innerText = galacticAge.saturnAge;
+  document.getElementById("saturnExpect").innerText = galacticAge.saturnAgeExpect;
+
+  document.getElementById("uranusAge").innerText = galacticAge.uranusAge;
+  document.getElementById("uranusExpect").innerText = galacticAge.uranusAgeExpect;
+
+  document.getElementById("neptuneAge").innerText = galacticAge.neptuneAge;
+  document.getElementById("neptuneExpect").innerText = galacticAge.neptuneAgeExpect;
+
+});
