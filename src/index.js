750346f4ada3ab773833a79e18a0273d36a43a2a
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-16 19:28:59 -0600

    fix the age display if surpased the life expectancy

--- src/index.js
+++ src/index.js
@@ -24,30 +24,34 @@ document.getElementById("user-form").addEventListener("submit", function(event) 
   let galacticAge = new GalacticAge(userAge, lifeExpect);
   galacticAge.calcAge();
   galacticAge.calcExpectancy();
+  if(parseInt(userAge) < parseInt(lifeExpect)) {
+    document.getElementById("mercuryExpect").innerText = galacticAge.mercuryAgeExpect;
+    document.getElementById("venusExpect").innerText = galacticAge.venusAgeExpect;
+    document.getElementById("earthExpect").innerText = lifeExpect - userAge;
+    document.getElementById("marsExpect").innerText = galacticAge.marsAgeExpect;
+    document.getElementById("jupiterExpect").innerText = galacticAge.jupiterAgeExpect;
+    document.getElementById("saturnExpect").innerText = galacticAge.saturnAgeExpect;
+    document.getElementById("uranusExpect").innerText = galacticAge.uranusAgeExpect;
+    document.getElementById("neptuneExpect").innerText = galacticAge.neptuneAgeExpect;
+  } else {
+    document.getElementById("mercuryExpect").innerText = galacticAge.mercuryAgeExpect + " years lived past life expectancy";
+    document.getElementById("venusExpect").innerText = galacticAge.venusAgeExpect + " years lived past life expectancy";
+    document.getElementById("earthExpect").innerText = Math.abs(lifeExpect - userAge) + " years lived past life expectancy";
+    document.getElementById("marsExpect").innerText = galacticAge.marsAgeExpect + " years lived past life expectancy";
+    document.getElementById("jupiterExpect").innerText = galacticAge.jupiterAgeExpect + " years lived past life expectancy";
+    document.getElementById("saturnExpect").innerText = galacticAge.saturnAgeExpect + " years lived past life expectancy";
+    document.getElementById("uranusExpect").innerText = galacticAge.uranusAgeExpect + " years lived past life expectancy";
+    document.getElementById("neptuneExpect").innerText = galacticAge.neptuneAgeExpect + " years lived past life expectancy";
+  }
 
   document.getElementById("mercuryAge").innerText = galacticAge.mercuryAge;
-  document.getElementById("mercuryExpect").innerText = galacticAge.mercuryAgeExpect;
-
   document.getElementById("venusAge").innerText = galacticAge.venusAge;
-  document.getElementById("venusExpect").innerText = galacticAge.venusAgeExpect;
-
-  document.getElementById("earthExpect").innerText = lifeExpect - userAge;
   document.getElementById("earthAge").innerText = userAge;
-
   document.getElementById("marsAge").innerText = galacticAge.marsAge;
-  document.getElementById("marsExpect").innerText = galacticAge.marsAgeExpect;
-
   document.getElementById("jupiterAge").innerText = galacticAge.jupiterAge;
-  document.getElementById("jupiterExpect").innerText = galacticAge.jupiterAgeExpect;
-
   document.getElementById("saturnAge").innerText = galacticAge.saturnAge;
-  document.getElementById("saturnExpect").innerText = galacticAge.saturnAgeExpect;
-
   document.getElementById("uranusAge").innerText = galacticAge.uranusAge;
-  document.getElementById("uranusExpect").innerText = galacticAge.uranusAgeExpect;
-
   document.getElementById("neptuneAge").innerText = galacticAge.neptuneAge;
-  document.getElementById("neptuneExpect").innerText = galacticAge.neptuneAgeExpect;
 
   document.getElementById("hidden").removeAttribute("class");
 });
