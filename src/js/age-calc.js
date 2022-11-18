8a2b50b0ee707ea1e68eb8ba071698edbcec715b
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-11-17 20:02:54 -0600

    add formula to calc expectancy if user lived past the life epec on Mercury

--- src/js/age-calc.js
+++ src/js/age-calc.js
@@ -15,12 +15,16 @@ export class GalacticAge {
   }
 
   calcExpectancy  () {
-    this.mercuryAgeExpect = Math.abs(((this.lifeExpectancy - this.age) * 365 / 88).toFixed(2));
-    this.venusAgeExpect = Math.abs(((this.lifeExpectancy - this.age) * 365 / 225).toFixed(2));
-    this.marsAgeExpect = Math.abs(((this.lifeExpectancy - this.age) * 365 / 687).toFixed(2));
-    this.jupiterAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 11.86).toFixed(2));
-    this.saturnAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 29.5).toFixed(2));
-    this.uranusAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 84).toFixed(2));
-    this.neptuneAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 165).toFixed(2));
+    if(this.lifeExpectancy >= this.age){
+      this.mercuryAgeExpect = Math.abs(((this.lifeExpectancy - this.age) * 365 / 88).toFixed(2));
+      this.venusAgeExpect = Math.abs(((this.lifeExpectancy - this.age) * 365 / 225).toFixed(2));
+      this.marsAgeExpect = Math.abs(((this.lifeExpectancy - this.age) * 365 / 687).toFixed(2));
+      this.jupiterAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 11.86).toFixed(2));
+      this.saturnAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 29.5).toFixed(2));
+      this.uranusAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 84).toFixed(2));
+      this.neptuneAgeExpect = Math.abs(((this.lifeExpectancy - this.age) / 165).toFixed(2));
+    } else {
+      this.mercuryAgeExpect = Math.abs(((this.age - this.lifeExpectancy) * 365 / 88).toFixed(2));
+    }
   }
 }
